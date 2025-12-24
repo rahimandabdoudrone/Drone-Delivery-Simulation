a984d4a2497b254b73f957bfac77653ecef857db
abderrahimboulariah <akadous90@gmail.com>
2025-12-24 17:48:58 +0100

    Update StandardDrone.java

--- model2/StandardDrone.java
+++ model2/StandardDrone.java
@@ -1,11 +1,10 @@
+package DroneHierarchi;
+import GEO.Position;
 public class StandardDrone extends Drone {
-
-    public StandardDrone(Position position) {
-        super(position, "StandardDrone", 30, 1.0);
-    }
-
-    @Override
-    public double calculateConsumption(double distance) {
-        return distance * 3; // 3% per km
-    }
+ public StandardDrone(Position pos){
+    super(pos,"Standard",30,1.0);
+ }
+ public double calculateConsumption(double distance){
+    return distance*3;
+ }
 }
