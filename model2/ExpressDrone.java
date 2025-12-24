0ec8fac075171c51e59ecb74c0d9509bd6e7a0fa
abderrahimboulariah <akadous90@gmail.com>
2025-12-24 17:47:12 +0100

    Update ExpressDrone.java

--- model2/ExpressDrone.java
+++ model2/ExpressDrone.java
@@ -1,11 +1,17 @@
+package DroneHierarchi;
+import GEO.Position;
 public class ExpressDrone extends Drone {
-
-    public ExpressDrone(Position position) {
-        super(position, "ExpressDrone", 45, 1.0);
+  public ExpressDrone(Position p){
+    super(p,"Express",45,1.0);
+  }
+  public double calculateConsumption(double distance){
+    return distance*4;
+  }
+  public double getspeed(){
+    if(this.getbattery()<20){
+      return 16;
+    }else{
+        return 45;
     }
-
-    @Override
-    public double calculateConsumption(double distance) {
-        return distance * 4; // 4% per km
-    }
+  }
 }
