955f5a8397310390bcd6354a12dd198a8795eb4b
abderrahimboulariah <akadous90@gmail.com>
2025-12-24 17:45:31 +0100

    Update NoFlyZone.java

--- model1/NoFlyZone.java
+++ model1/NoFlyZone.java
@@ -1,14 +1,13 @@
-public class NoFlyZone {
-
+package GEO;
+public class NoFlyZone{
     private Position center;
     private double radius;
 
-    public NoFlyZone(Position center, double radius) {
-        this.center = center;
-        this.radius = radius;
+    public NoFlyZone(Position center,double radius){
+        this.center=center;
+        this.radius=radius;
     }
-
-    public boolean contains(Position p) {
-        return center.distanceTo(p) <= radius;
+    public boolean contains(Position p){
+        return this.center.distanceTo(p)<=this.radius;
     }
 }
